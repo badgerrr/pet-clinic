ca20fd614786ddb932d8e9e806ba118968364dac
Jack Arthurton <jack.arthurton@simplybusiness.co.uk>
2021-01-01 14:32:52 +0000

    adding veta page and controller

---
+++ pet-clinic-web/src/main/java/learning/springframework/petclinic/controllers/VetIndexController.java
@@ -0,0 +1,13 @@
+package learning.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetIndexController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
